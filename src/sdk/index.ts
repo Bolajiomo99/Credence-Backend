c5560e78e4d0c27e3d69e8692b6fa15711ee3c1d
OluRemiFour <lekanayoola04@gmail.com>
2026-06-01 09:38:08 +0100

    build(grpc): add buf contracts with breaking-change CI gate

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -1,3 +1,4 @@
+// REST/HTTP client
 export { CredenceClient } from './client.js'
 export {
   CredenceConfig,
@@ -8,3 +9,15 @@ export {
   AttestationsResponse,
   VerificationProof,
 } from './types.js'
+
+// Internal gRPC client (Connect-RPC)
+// Requires: npm install @bufbuild/protobuf @connectrpc/connect @connectrpc/connect-node
+// Requires: buf generate  (populates src/sdk/grpc/gen/)
+export {
+  createCredenceGrpcClient,
+  type CredenceGrpcClient,
+  type CredenceGrpcConfig,
+  createSharedSecretInterceptor,
+  createRequestIdInterceptor,
+  INTERNAL_TOKEN_HEADER,
+} from './grpc/index.js'
